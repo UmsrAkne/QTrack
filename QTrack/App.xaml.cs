4a99a7ad9342ed8b3a64af78e90faaeb88074cdb
UmsrAkne <sibata486@gmail.com>
2026-09-05 23:56:20 +0900

    init

---
+++ QTrack/App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using QTrack.Views;
+
+namespace QTrack;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+}
---
+++ QTrack/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QTrack.Utils;
+
+namespace QTrack.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        private string title = "QTrack";
+
+        public MainWindowViewModel()
+        {
+            AppLogger.Info("MainWindowViewModel created");
+        }
+
+        public string Title { get => title; set => SetProperty(ref title, value); }
+    }
+}
