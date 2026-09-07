d64eac518e99651813f3264ccabdce8bc850ae65
UmsrAkne <sibata486@gmail.com>
2026-09-08 00:10:08 +0900

    feat: Display mock issues in `IssuesView`
    - Implemented mock issue generation in `MockIssueService` for testing purposes.
    - Bound `Issues` collection in `IssuesViewModel` to `ListBox` in `IssuesView`.
    - Added `Summary` display for issues in `IssuesView`.

--- QTrack/ViewModels/IssuesViewModel.cs
+++ QTrack/ViewModels/IssuesViewModel.cs
@@ -1,4 +1,5 @@
-﻿using QTrack.Models;
+﻿using System.Collections.ObjectModel;
+using QTrack.Models;
 using QTrack.Services;
 using QTrack.Utils;
 
@@ -23,10 +24,16 @@ namespace QTrack.ViewModels
 
         public Project? CurrentProject { get; set; }
 
+        public ObservableCollection<Issue> Issues { get; set; } = new ();
+
         public async Task InitializeAsync(Project project)
         {
             await Task.Delay(2000);
             CurrentProject = project;
+
+            var issues = issueService.GetIssues(project, 10);
+            Issues.AddRange(issues);
+
             Header = $"{project.Name} の課題";
         }
     }
