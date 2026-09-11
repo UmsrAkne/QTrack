e7568e731e1eac20879c7e1ea00d590f57fd4672
UmsrAkne <sibata486@gmail.com>
2026-09-11 19:46:56 +0900

    feat: Add `StateToggleButton` control and integrate it into `IssuesView`
    - Introduced `StateToggleButton` XAML control with style triggers for issue state transitions (`Created`, `InProgress`, `Pausing`, `Completed`, and `Obsolete`).
    - Implemented state transition logic in `StateToggleButton.xaml.cs` using commands and dependency properties.
    - Replaced status button in `IssuesView` with the new `StateToggleButton` bound to the `State` property of issues.

---
+++ QTrack/Views/Controls/StateToggleButton.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using QTrack.Models;
+
+namespace QTrack.Views.Controls
+{
+    public partial class StateToggleButton
+    {
+        public readonly static DependencyProperty StateProperty =
+            DependencyProperty.Register(
+                nameof(State),
+                typeof(IssueState),
+                typeof(StateToggleButton),
+                new FrameworkPropertyMetadata(IssueState.Created, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public readonly static DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(StateToggleButton));
+
+        public StateToggleButton()
+        {
+            InitializeComponent();
+        }
+
+        public IssueState State
+        {
+            get => (IssueState)GetValue(StateProperty);
+            set => SetValue(StateProperty, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            // 状態遷移のロジック
+            State = State switch
+            {
+                IssueState.Created => IssueState.InProgress,
+                IssueState.InProgress => IssueState.Pausing,
+                IssueState.Pausing => IssueState.InProgress,
+                _ => State,
+            };
+        }
+    }
+}
